37ca7be9572dbc7aaf01a34ad7c454cbd4249310
Christos Pontikis <pontikis@gmail.com>
2013-02-22 12:36:10 +0200

    schema

--- schema/small_blog.sql
+++ schema/small_blog.sql
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ctg` varchar(254) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `posts`
 --
 
@@ -33,12 +56,23 @@ CREATE TABLE `posts` (
   `tags` varchar(254) DEFAULT NULL,
   `impressions` int(11) DEFAULT '0',
   `date_updated` varchar(14) DEFAULT NULL,
+  `categories_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `posts_ix1` (`url`),
   KEY `posts_ix2` (`post_title`),
   KEY `posts_ix3` (`date_published`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'/test-url1','post1 title','post1 sub_title','20130201000000','20120110000000',NULL,'tag1, tag2, tag3',43,NULL,NULL),(2,'/test-url2','post2 title',NULL,'20130201000000','20130115000000',NULL,NULL,20,NULL,NULL),(3,'/test-url3','post3 title',NULL,'20130201000000','20130220000000',NULL,NULL,17,NULL,NULL);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tags`
@@ -55,6 +89,15 @@ CREATE TABLE `tags` (
   KEY `tags_ix1` (`tag`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -65,4 +108,4 @@ CREATE TABLE `tags` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-02-18 14:08:11
+-- Dump completed on 2013-02-22 12:35:52
